7db7969c9d43ae93bc8cb989f5c40e3d8e713eff
arcanar7 <arcanar7@gmail.com>
2020-03-04 16:13:19 +0400

    init: start calculator task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,75 @@
 function eval() {
-    // Do not use eval!!!
-    return;
+  // Do not use eval!!!
+  return
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+  const openBrackets = expr.match(/[(]/g)
+  const closeBrackets = expr.match(/[)]/g)
+  let exprArray = expr.match(/[()\*\/+-]|\d+/g)
+
+  if ((!openBrackets && closeBrackets) || (openBrackets && !closeBrackets))
+    throw "ExpressionError: Brackets must be paired"
+  if (openBrackets && closeBrackets) {
+    if (openBrackets.length !== closeBrackets.length) {
+      throw "ExpressionError: Brackets must be paired"
+    }
+  }
+
+  let stackNumbers = []
+  let stackOperators = []
+
+  for (item of exprArray) {
+    if (!isNaN(item)) {
+      stackNumbers.push(+item)
+    } else {
+      if (priority[item]) {
+        if (priority[item] <= priority[lastItemInStack(stackOperators)]) {
+          stackNumbers.push(
+            operations[lastItemInStack(stackOperators)](
+              stackNumbers.pop(),
+              stackNumbers.pop()
+            )
+          )
+        }
+      }
+      stackOperators.push(item)
+    }
+  }
+
+  while (stackNumbers.length > 1) {
+    let [b, a] = [stackNumbers.pop(), stackNumbers.pop()]
+    stackNumbers.push(operations[lastItemInStack(stackOperators)](a, b))
+  }
+
+  return stackNumbers.pop()
 }
 
 module.exports = {
-    expressionCalculator
+  expressionCalculator
 }
+
+const operations = {
+  "+": (a, b) => a + b,
+  "-": (a, b) => a - b,
+  "*": (a, b) => a * b,
+  "/": (a, b) => {
+    if (b === 0) throw new TypeError("TypeError: Division by zero.")
+    return a / b
+  }
+}
+
+const priority = {
+  "+": 1,
+  "-": 1,
+  "*": 2,
+  "/": 2,
+  "0": 0
+}
+
+function lastItemInStack(stack) {
+  return stack ? stack[stack.length - 1] : 0
+}
+
+const expr = " 49 * 63 / 58 * 36"
+console.log(expressionCalculator(expr))
